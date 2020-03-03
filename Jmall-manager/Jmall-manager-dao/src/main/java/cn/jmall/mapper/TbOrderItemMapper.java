bb1bb4bda2ad2ca2f402a5a5e4775744bd1b37e1
Memories6 <1371910434@qq.com>
2020-03-03 17:22:53 +0800

    根据mybatis逆向生成POJO和mapper

---
+++ Jmall-manager/Jmall-manager-dao/src/main/java/cn/jmall/mapper/TbOrderItemMapper.java
@@ -0,0 +1,30 @@
+package cn.jmall.mapper;
+
+import cn.jmall.pojo.TbOrderItem;
+import cn.jmall.pojo.TbOrderItemExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbOrderItemMapper {
+    int countByExample(TbOrderItemExample example);
+
+    int deleteByExample(TbOrderItemExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(TbOrderItem record);
+
+    int insertSelective(TbOrderItem record);
+
+    List<TbOrderItem> selectByExample(TbOrderItemExample example);
+
+    TbOrderItem selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") TbOrderItem record, @Param("example") TbOrderItemExample example);
+
+    int updateByExample(@Param("record") TbOrderItem record, @Param("example") TbOrderItemExample example);
+
+    int updateByPrimaryKeySelective(TbOrderItem record);
+
+    int updateByPrimaryKey(TbOrderItem record);
+}
