b5496862c50ca5e9c32ce51b55fb41e61128ac8d
Memories6 <1371910434@qq.com>
2020-03-07 19:49:24 +0800

    内容管理-展示页面数据获取

--- Jmall-portal-web/src/main/java/cn/jmall/portal/controller/IndexController.java
+++ Jmall-portal-web/src/main/java/cn/jmall/portal/controller/IndexController.java
@@ -1,7 +1,15 @@
 package cn.jmall.portal.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.jmall.content.service.ContentService;
+import cn.jmall.pojo.TbContent;
 
 /**
  * 首页展示Controller
@@ -12,8 +20,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+	@Value("${CONTENT_LUNBO_ID}")
+	private Long CONTENT_LUNBO_ID;
+	
+	@Autowired
+	private ContentService contentService;
+	
 	@RequestMapping("/index")
-	public String showIndex() {
+	public String showIndex(Model model) {
+		// 查询内容列表
+		List<TbContent> ad1List = contentService.getContentListByCid(CONTENT_LUNBO_ID);
+		// 将结果传递给页面
+		model.addAttribute("ad1List", ad1List);
 		
 		return "index";
 	}
