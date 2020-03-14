cc70e0e18cf8fb8c2716caa96cc635834512d8d4
Memories6 <1371910434@qq.com>
2020-03-14 13:29:22 +0800

    cart-加入购物车功能

---
+++ Jmall-cart-web/src/main/java/cn/jmall/cart/controller/cartController.java
@@ -0,0 +1,103 @@
+package cn.jmall.cart.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cn.jmall.common.util.CookieUtils;
+import cn.jmall.common.util.JsonUtils;
+import cn.jmall.pojo.TbItem;
+import cn.jmall.service.ItemService;
+
+/**
+ * 购物车处理Controller
+ * 
+ * @author Jmall
+ * @CreateDate 2020年3月14日
+ * @Description
+ */
+@Controller
+public class cartController {
+
+	@Autowired
+	private ItemService itemService;
+	@Value("${COOKIE_CART_EXPIRE}")
+	private Integer COOKIE_CART_EXPIRE;
+
+	@RequestMapping("cart/add/{itemId}")
+	public String addCart(@PathVariable Long itemId, @RequestParam(defaultValue = "1") Integer num,
+			HttpServletRequest request, HttpServletResponse response) {
+		// 从cookie中取购物车列表
+		List<TbItem> cartList = getCartListFromCookie(request);
+		boolean flag = false;
+		// 判断商品在商品列表中是否存在
+		for (TbItem tbItem : cartList) {
+			if (tbItem.getId() == itemId.longValue()) {
+				// 如果存在，商品数量相加
+				tbItem.setNum(tbItem.getNum() + num);
+				flag = true;
+				// 跳出循环
+				break;
+			}
+		}
+
+		// 不存在
+		if (!flag) {
+			// 根据商品id查询商品信息，得到一个TBItem对象
+			TbItem tbItem = itemService.getItemById(itemId);
+			// 设置商品数量
+			tbItem.setNum(num);
+			// 取一张图片
+			String image = tbItem.getImage();
+			if(StringUtils.isNotBlank(image)) {
+				tbItem.setImage(image.split(",")[0]);
+			}
+			// 把商品调价到商品列表
+			cartList.add(tbItem);
+		}
+		// 写入cookie
+		CookieUtils.setCookie(request, response, "cart", JsonUtils.objectToJson(cartList), COOKIE_CART_EXPIRE, true);
+		// 返回添加成功页面
+		return "cartSuccess";
+	}
+
+	@RequestMapping("/cart/cart")
+	public String showCatList(HttpServletRequest request) {
+//		从cookie中取购物车列表
+		List<TbItem> cartList = getCartListFromCookie(request);
+//		把列表传递给页面
+		request.setAttribute("cartList", cartList);
+//		返回逻辑视图
+		return "cart";
+	}
+	
+	/**
+	 * 从cookie中取得购物车列表的处理
+	 * @param request
+	 * @return
+	 */
+	private List<TbItem> getCartListFromCookie(HttpServletRequest request) {
+		// 获取cookie中购物车商品信息
+		String json = CookieUtils.getCookieValue(request, "cart", true);
+		// 判断json是否为空
+		if (StringUtils.isBlank(json)) {
+			return new ArrayList<>();
+		}
+
+		// 把json转换成商品列表
+		List<TbItem> list = JsonUtils.jsonToList(json, TbItem.class);
+		return list;
+	}
+
+}
