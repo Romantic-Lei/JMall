a2df7055aae3e693a8562148e7c1c7c8771de32a
Memories6 <1371910434@qq.com>
2020-04-01 16:27:15 +0800

    Crypt 商家密码加密

--- luojia/luojia-parent/easyBuy-shop-web/src/main/java/com/easybuy/shop/controller/SellerController.java
+++ luojia/luojia-parent/easyBuy-shop-web/src/main/java/com/easybuy/shop/controller/SellerController.java
@@ -1,6 +1,7 @@
 package com.easybuy.shop.controller;
 import java.util.List;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,11 @@ public class SellerController {
 	 */
 	@RequestMapping("/add")
 	public Result add(@RequestBody TbSeller seller){
+		// 密码加密
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		String password = bCryptPasswordEncoder.encode(seller.getPassword());
+		seller.setPassword(password);
+		
 		try {
 			sellerService.add(seller);
 			return new Result(true, "增加成功");
