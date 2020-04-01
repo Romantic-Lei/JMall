bc276c99b1f5a3fb7fc825f9cbf25d91dc61f57e
Memories6 <1371910434@qq.com>
2020-04-01 17:10:37 +0800

    用户名显示及退出登录

---
+++ luojia/luojia-parent/easyBuy-shop-web/src/main/webapp/js/controller/loginController.js
@@ -0,0 +1,13 @@
+app.controller('loginController',function($scope, loginService){
+	
+	// 显示当前用户名
+	$scope.showLoginName=function(){
+		loginService.loginName().success(
+			function(response){
+				$scope.loginName=response.loginName;
+			}
+		);
+	}
+	
+	
+});
---
+++ luojia/luojia-parent/easyBuy-shop-web/src/main/webapp/js/service/loginService.js
@@ -0,0 +1,8 @@
+//登录服务 
+app.service('loginService',function($http){
+	
+	this.loginName=function(){
+		return $http.get('../login/name.do');
+	}
+	
+});
