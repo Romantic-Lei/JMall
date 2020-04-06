d63e4414aaa1ae33be7c1214a9279e9b2182ef63
Memories6 <1371910434@qq.com>
2020-04-06 11:17:37 +0800

    配置图片服务器

---
+++ luojia/luojia-parent/easyBuy-manager-web/src/main/java/com/easybuy/manager/controller/UploadController.java
@@ -0,0 +1,42 @@
+package com.easybuy.manager.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.easybuy.entity.Result;
+
+import util.FastDFSClient;
+
+
+@RestController
+public class UploadController {
+
+	@Value("${FILE_SERVER_URL}")
+	private String FILE_SERVER_URL;
+	
+	@RequestMapping("/upload")
+	public Result upload(MultipartFile file) {
+
+		try {
+			// 原始文件名
+			String originalFilename = file.getOriginalFilename();
+			// 得到上传文件的扩展名
+			String extName = originalFilename.substring(originalFilename.indexOf(".") + 1);
+			
+			// 创建一个工具类客户端
+			FastDFSClient client = new FastDFSClient("classpath:config/fdfs_client.conf");
+
+			String filePath = client.uploadFile(file.getBytes(), extName);
+			
+			return new Result(true, FILE_SERVER_URL + filePath);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			return new Result(false, "上传失败");
+		}
+	}
+
+}
--- luojia/luojia-parent/easyBuy-manager-web/src/main/webapp/js/controller/contentController.js
+++ luojia/luojia-parent/easyBuy-manager-web/src/main/webapp/js/controller/contentController.js
@@ -1,5 +1,5 @@
  //控制层 
-app.controller('contentController' ,function($scope,$controller   ,contentService,uploadService,contentCategoryService){	
+app.controller('contentController' ,function($scope,$controller   ,contentService,uploadService,contentCategoryService,uploadService){	
 	
 	$controller('baseController',{$scope:$scope});//继承
 	
