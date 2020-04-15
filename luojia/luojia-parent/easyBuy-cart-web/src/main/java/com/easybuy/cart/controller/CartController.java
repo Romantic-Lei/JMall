92f02c9d6e6be15fa2c57a0d983434c1a99a1e8a
Memories6 <1371910434@qq.com>
2020-04-15 22:37:56 +0800

    购物车控制层

---
+++ luojia/luojia-parent/easyBuy-cart-web/src/main/java/com/easybuy/cart/controller/CartController.java
@@ -0,0 +1,63 @@
+package com.easybuy.cart.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
+import com.easybuy.cart.service.CartService;
+import com.easybuy.entity.Result;
+import com.easybuy.pojogroup.Cart;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+	@Reference
+	private CartService cartService;
+
+	/**
+	 * 添加商品到购物车
+	 * 
+	 * @param itemId
+	 * @param num
+	 * @return
+	 */
+	@RequestMapping("/addGoodToCartList")
+	public Result addGoodToCartList(Long itemId, Integer num, HttpServletRequest request,
+			HttpServletResponse response) {
+
+		try {
+			List<Cart> cartList = findCartList(request);
+			// 添加商品到购物车
+			cartList = cartService.addGoodsToCartList(cartList, itemId, num);
+			// 将购物车json字符串存入cookie
+			util.CookieUtil.setCookie(request, response, "cartList", JSON.toJSONString(cartList));
+
+			return new Result(true, "添加购物车成功");
+		} catch (RuntimeException e) {
+
+			return new Result(false, e.getMessage());
+		} catch (Exception e) {
+			return new Result(false, "添加购物车失败");
+		}
+	}
+
+	@RequestMapping("/findCartList")
+	public List<Cart> findCartList(HttpServletRequest request) {
+		// 在cookie中提取购物车列表json字符串
+		String cartListJson = util.CookieUtil.getCookieValue(request, "cartList");
+
+		if (cartListJson == null || cartListJson.equals("")) {
+			cartListJson = "[]";
+		}
+		// 获取购物车列表
+		return JSON.parseArray(cartListJson, Cart.class);
+	}
+
+}
