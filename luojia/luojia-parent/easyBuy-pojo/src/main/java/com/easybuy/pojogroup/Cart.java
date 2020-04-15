f04eecffaa409663e56fdeb40ebe6598315a48c0
Memories6 <1371910434@qq.com>
2020-04-15 17:19:26 +0800

    购物车商品明细实体类

---
+++ luojia/luojia-parent/easyBuy-pojo/src/main/java/com/easybuy/pojogroup/Cart.java
@@ -0,0 +1,37 @@
+package com.easybuy.pojogroup;
+
+import java.util.List;
+
+import com.easybuy.pojo.TbOrderItem;
+
+public class Cart {
+
+	private String sellerId; // 商家id
+	private String sellerName; // 商家名称
+	private List<TbOrderItem> orderItemList; // 商品集合
+
+	public String getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(String sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public List<TbOrderItem> getOrderItemList() {
+		return orderItemList;
+	}
+
+	public void setOrderItemList(List<TbOrderItem> orderItemList) {
+		this.orderItemList = orderItemList;
+	}
+
+}
