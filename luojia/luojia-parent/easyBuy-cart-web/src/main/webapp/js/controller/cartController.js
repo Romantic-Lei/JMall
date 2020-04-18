75da9253bc907761e89f52c1ea3969c955039b04
Memories6 <1371910434@qq.com>
2020-04-18 16:02:48 +0800

    商品件数，价格总合计

--- luojia/luojia-parent/easyBuy-cart-web/src/main/webapp/js/controller/cartController.js
+++ luojia/luojia-parent/easyBuy-cart-web/src/main/webapp/js/controller/cartController.js
@@ -1,10 +1,12 @@
 // 购物车控制层
 app.controller('cartController', function($scope, cartService){
 	
+	//查询购物车列表
 	$scope.findCartList=function(){
 		cartService.findCartList().success(
 			function(response){
 				$scope.cartList=response;
+				sum();//求合计
 				console.log($scope.cartList);
 			}
 		);
@@ -21,6 +23,21 @@ app.controller('cartController', function($scope, cartService){
 				}
 			}
 		);
+	}
+	
+	//求合计
+	sum=function(){
+		$scope.totalNum=0;//数量合计
+		$scope.totalMoney=0.00;//金额合计
+		
+		for(var i=0;i<$scope.cartList.length;i++){
+			var cart = $scope.cartList[i];			
+			for(var j=0;j<cart.orderItemList.length;j++){
+				var orderItem=cart.orderItemList[j];
+				$scope.totalNum+=orderItem.num;
+				$scope.totalMoney+=orderItem.totalFee;				
+			}				
+		}	
 	}
 	
 });
