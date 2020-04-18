f95b5fbd0177d32808e7322bcc09863a8aaf1b1e
Memories6 <1371910434@qq.com>
2020-04-18 22:20:11 +0800

    订单结算金额合计

--- luojia/luojia-parent/easyBuy-cart-web/src/main/webapp/js/controller/orderInfoController.js
+++ luojia/luojia-parent/easyBuy-cart-web/src/main/webapp/js/controller/orderInfoController.js
@@ -39,16 +39,33 @@ app.controller('orderInfoController', function($scope, addressService, cartServi
 	$scope.order = {
 		paymentType : '1'
 	};// 订单对象
+	
 	// 选择支付类型
 	$scope.selectPayType = function(type) {
 		$scope.order.paymentType = type;
 	}
 
-	// 添加商品到购物车
+	// 读取购物车列表
 	$scope.findCartList = function() {
 		cartService.findCartList().success(function(response) {
 			$scope.cartList = response;
+			sum();
 		});
+	}
+	
+	//求合计
+	sum=function(){
+		$scope.totalNum=0;//数量合计
+		$scope.totalMoney=0.00;//金额合计
+		
+		for(var i=0;i<$scope.cartList.length;i++){
+			var cart = $scope.cartList[i];			
+			for(var j=0;j<cart.orderItemList.length;j++){
+				var orderItem=cart.orderItemList[j];
+				$scope.totalNum+=orderItem.num;
+				$scope.totalMoney+=orderItem.totalFee;				
+			}				
+		}	
 	}
 
 });
