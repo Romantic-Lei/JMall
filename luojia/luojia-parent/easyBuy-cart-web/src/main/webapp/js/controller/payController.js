2ac23bf3772f35c11679ab36d32de7873d02597e
Memories6 <1371910434@qq.com>
2020-04-25 00:03:27 +0800

    数据准备

--- luojia/luojia-parent/easyBuy-cart-web/src/main/webapp/js/controller/payController.js
+++ luojia/luojia-parent/easyBuy-cart-web/src/main/webapp/js/controller/payController.js
@@ -1,22 +1,23 @@
-// 购物车控制层
+// 支付控制层
 app.controller('payController', function($scope, payService){
 	
 	$scope.createNative = function(){
-		payService.createNative().success({
+		payService.createNative().success(
 			function(response){
 				if(response.form != null){
+					alert("执行前");
 					var ewm = qrcode(10, "H");
-					ewm.addData(response.form);// 生成二维码数据
-					ewm.make();
-					document.getElementById("qr").innerHtml=ewm.createImgTag();
+//					ewm.addData(response.form);// 生成二维码数据
+//					ewm.make();
+					document.getElementById("qr").innerHTML=response.form;
 					$scope.out_trade_no = response.out_trade_no;// 订单号
 					$scope.total_amount = (response.total_amount/100).toFixed(2);// 订单金额
-					
+					alert("执行后");
 				} else {
 					alert("生成二维码发生错误");
 				}
 			}
-		});
+		);
 	}
 	
 });
