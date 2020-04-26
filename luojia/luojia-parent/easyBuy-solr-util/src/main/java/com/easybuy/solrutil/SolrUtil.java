7d1505b3a02f68ec961bcc9a5ea4b95782cfc4c7
Memories6 <1371910434@qq.com>
2020-04-26 16:24:31 +0800

    数据导入

---
+++ luojia/luojia-parent/easyBuy-solr-util/src/main/java/com/easybuy/solrutil/SolrUtil.java
@@ -0,0 +1,42 @@
+package com.easybuy.solrutil;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import com.easybuy.mapper.TbItemMapper;
+import com.easybuy.pojo.TbItem;
+import com.easybuy.pojo.TbItemExample;
+import com.easybuy.pojo.TbItemExample.Criteria;
+
+@Component
+public class SolrUtil {
+	
+	@Autowired
+	private TbItemMapper itemMapper;
+
+	public static void main(String[] args) {
+		
+		ApplicationContext content=new ClassPathXmlApplicationContext("classpath*:spring/applicationContext*.xml");
+		SolrUtil solrUtil=  (SolrUtil) content.getBean("solrUtil");
+		solrUtil.importData();
+	}
+	
+	// 导入数据
+	public void importData() {
+		TbItemExample example = new TbItemExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andStatusEqualTo("1"); // 状态为1的才可以查询
+		
+		List<TbItem> list = itemMapper.selectByExample(example);
+		
+		for (TbItem item : list) {
+			System.out.println(item.getTitle());
+		}
+		
+	}
+	
+}
