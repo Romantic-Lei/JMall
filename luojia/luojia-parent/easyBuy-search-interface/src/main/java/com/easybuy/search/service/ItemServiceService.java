032c3b045841171a4047a7c50eab22f41f41b7c0
Memories6 <1371910434@qq.com>
2020-04-26 22:37:36 +0800

    搜索服务接口

---
+++ luojia/luojia-parent/easyBuy-search-interface/src/main/java/com/easybuy/search/service/ItemServiceService.java
@@ -0,0 +1,16 @@
+package com.easybuy.search.service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 搜索服务接口
+ * @author Romantic
+ * @CreateDate 2020年4月26日
+ * @Description
+ */
+public interface ItemServiceService {
+
+	public Map<String, Object> search(Map<String, Object> searchMap);
+	
+}
