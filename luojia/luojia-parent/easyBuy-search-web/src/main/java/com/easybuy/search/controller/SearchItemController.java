f03b9b2a4443df3e46bea776f0950ae7a719dc3b
Memories6 <1371910434@qq.com>
2020-04-27 22:54:51 +0800

    sorl 控制层实现

---
+++ luojia/luojia-parent/easyBuy-search-web/src/main/java/com/easybuy/search/controller/SearchItemController.java
@@ -0,0 +1,24 @@
+package com.easybuy.search.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.easybuy.search.service.ItemSearchService;
+
+@RestController
+@RequestMapping("/itemsearch")
+public class SearchItemController {
+
+	@Reference
+	private ItemSearchService itemSearchService;
+	
+	@RequestMapping("/search")
+	public Map<String,Object> searchMap(Map<String, Object> map){
+		
+		return itemSearchService.search(map);
+	}
+	
+}
