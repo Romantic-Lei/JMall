7a285b1833806eace07a7200a511afcc8e562411
Memories6 <1371910434@qq.com>
2020-05-01 15:22:36 +0800

    分类列表查询后端代码

--- luojia/luojia-parent/easyBuy-search-service/src/main/java/com/easybuy/search/service/impl/ItemSearchServiceImpl.java
+++ luojia/luojia-parent/easyBuy-search-service/src/main/java/com/easybuy/search/service/impl/ItemSearchServiceImpl.java
@@ -1,13 +1,20 @@
 package com.easybuy.search.service.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.core.query.Criteria;
+import org.springframework.data.solr.core.query.GroupOptions;
 import org.springframework.data.solr.core.query.Query;
 import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.result.GroupEntry;
+import org.springframework.data.solr.core.query.result.GroupPage;
+import org.springframework.data.solr.core.query.result.GroupResult;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 
 import com.alibaba.dubbo.config.annotation.Service;
@@ -30,14 +37,30 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 	@Override
 	public Map<String, Object> search(Map<String, Object> searchMap) {
 
+		Map<String, Object> resultMap = new HashMap<>();
+		Query query = new SimpleQuery();
+		// 1.根据关键字查询商品列表
 		Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
-
-		Query query = new SimpleQuery(criteria);
-
+		query.addCriteria(criteria);
 		ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
 
-		Map<String, Object> resultMap = new HashMap<>();
 		resultMap.put("rows", page.getContent());
+
+		// 2.根据关键字查询分类列表（分组查询 类似sql的 group by）
+
+		GroupOptions groupOptions = new GroupOptions().addGroupByField("item_category");
+		query.setGroupOptions(groupOptions);
+		GroupPage<TbItem> groupPage = solrTemplate.queryForGroupPage(query, TbItem.class);
+		GroupResult<TbItem> groupResult = groupPage.getGroupResult("item_category");
+		Page<GroupEntry<TbItem>> groupEntries = groupResult.getGroupEntries();
+		List<GroupEntry<TbItem>> cataList = groupEntries.getContent();
+
+		List<String> categoryList = new ArrayList<String>();
+		for (GroupEntry<TbItem> entry : cataList) {
+			categoryList.add(entry.getGroupValue());
+		}
+
+		resultMap.put("categoryList", categoryList);
 
 		return resultMap;
 	}
